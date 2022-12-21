856b1b07d3ba8489ea4767732765494dca4ba7d8
farhan <farhanafriza24@gmail.com>
2022-12-21 11:29:47 +0700

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.layout')
+
+@section('header')
+    @include('templates.header')
+@endsection
+
+@section('content')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Dashboard</h1>
+    </div>
+    <div class="row row-col-4">
+        <div class="col-md-4">
+            @include('components.card-dashboard', ['title' => 5, 'subtitle' => 'Jumlah pesanan hari ini', 'icon' => 'receipt'])
+        </div>
+        <div class="col-md-4">
+            @include('components.card-dashboard', ['title' => 'Rp. 150.000', 'subtitle' => 'Total penjualan hari ini', 'icon' => 'receipt_long'])
+        </div>
+        <div class="col-md-4">
+            @include('components.card-dashboard', ['title' => 'Rp. 3.500.000', 'subtitle' => 'Total penjualan bulan ini', 'icon' => 'insights'])
+        </div>
+    </div>
+@endsection
+
+@section('footer')
+    @include('templates.footer')
+@endsection
---
+++ resources/views/components/card-dashboard.blade.php
@@ -0,0 +1,17 @@
+<div class="card">
+    <div class="card-body">
+        <div class="row row-col-2">
+            <div class="col">
+                <div class="text-bold fs-2">
+                    {{ $title }}
+                </div>
+                <div class="fs-4">
+                    {{ $subtitle }}
+                </div>
+            </div>
+            <div class="col text-center flex justify-content-center align-items-center">
+                <span class="material-icons" style="font-size: 84px">{{ $icon }}</span>
+            </div>
+        </div>
+    </div>
+</div>
